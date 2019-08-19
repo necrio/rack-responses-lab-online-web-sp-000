08fa10e3cd0661bfaa76446cca4157ac256fe3fd
Keith Isham <kisham8899@gmail.com>
2019-08-19 09:27:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,14 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
- T = Time.now.hour
- 
 
  
  
-    if T.between?(0,11)
+    if Time.now.hour.between?(0,11)
       resp.write "good morning"
-    else T.between?(12, 23)
+    else Time.now.hour.between?(12, 23)
       resp.write "good afternoon"
     end
  
