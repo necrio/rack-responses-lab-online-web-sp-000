67fb0dc158a2d887f5514904f1e5687353f501f9
Keith Isham <kisham8899@gmail.com>
2019-08-19 09:29:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if Time.now.hour.between?(0,11)
       resp.write "Good Morning!"
     else Time.now.hour.between?(12, 24)
-      resp.write "good afternoon"
+      resp.write "good Afternoon"
     end
  
     resp.finish
